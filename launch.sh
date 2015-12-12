153e8f25f882c75bbcda39c47395898db9b8efbc
grantngai <pngai@hawk.iit.edu>
2015-12-12 02:35:51 -0600

    added launch.sh

--- launch.sh
+++ launch.sh
@@ -16,14 +16,14 @@ for i in {0..60}; do echo -ne '.'; sleep 1;done
 
 aws elb register-instances-with-load-balancer --load-balancer-name itmo444pngai-lb --instances ${instanceARR[@]}
 
-aws elb configure-health-check --load-balancer-name itmo444pngai-lb --health-check Target=HTTP:80/index.html,interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+aws elb configure-health-check --load-balancer-name itmo444pngai-lb --health-check Target=HTTP:80/index.html,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
 
 echo -3 "\nWaiting an additional 60 seconds - before opening the ELB in a webbrowser"
 for i in {0..60}; do echo -ne '.'; sleep 1;done
 
-aws autoscaling create-launch-configuration --launch-configuration-name itmo444-launch-config --image-id $1 --key-name $6 --security-groups $4 --instance-type $3 --user-data file://../itmo444-env/install-webserver,sh --iam-instance-profile $7
+aws autoscaling create-launch-configuration --launch-configuration-name itmo444-launch-config --image-id $1 --key-name $6 --security-groups $4 --instance-type $3 --user-data file://../itmo-444-env/install-webserver.sh --iam-instance-profile $7
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo444-extended-auto-scaling-group-2 --launch-configuration-name itmo444-launch-config --itmo444pngai-lb --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $5
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo444-extended-auto-scaling-group-2 --launch-configuration-name itmo444-launch-config --load-balancer-names itmo444pngai-lb --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $5
 
 mapfile -t dbinstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g")
 
